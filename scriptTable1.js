0c655d9b86f81e18f8046fb36642782d17d74b6a
Vinicius Coelho <vinicius2001@gmail.com>
2021-07-25 17:21:07 -0300

    Segundo dia commit 2

--- scriptTable1.js
+++ scriptTable1.js
@@ -1,11 +1,13 @@
 // Project Table
 var selectedRow = null
 
+// Passa os dados da variável para a tabela 
 function onFormSubmit() {
     var formData = readFormData();
     insertNewRecord(formData);
 }
 
+// Le os dados do formulário e armazena na variável
 function readFormData() {
     var formData = {}
     formData["projectName"] = document.getElementById("projectName").value;
@@ -14,6 +16,7 @@ function readFormData() {
     return formData;
 }
 
+// Insere uma nova linha na tabela
 function insertNewRecord(data) {
     var table = document.getElementById("projectList").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
@@ -25,13 +28,17 @@ function insertNewRecord(data) {
     cell2.innerHTML = data.initialDate;
     cell3 = newRow.insertCell(3);
     cell3.innerHTML = data.finalDate;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = "--";
+    cell5 = newRow.insertCell(5);
+    cell5.innerHTML = "--";
     resetForm();
 }
 
+// Reseta o formulário
 function resetForm() {
     document.getElementById("projectName").value = "";
     document.getElementById("initialDate").value = "";
     document.getElementById("finalDate").value = "";
     selectedRow = null;
 }
-
